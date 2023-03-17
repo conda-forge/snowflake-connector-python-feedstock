ec12c2e608a48bc6c6a473c1d909b36b3691e86d
Uwe L. Korn <uwe.korn@quantco.com>
2023-03-17 10:53:34 +0100

    Add BUILD_PREFIX pyarrow to LD_LIBRARY_PATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,10 @@
 
 set -exuo pipefail
 
+if [[ "${build_platform}" != "${target_platform}" ]]; then
+  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:${SP_DIR//$PREFIX/$BUILD_PREFIX}/pyarrow/"
+fi
+
 export SF_NO_COPY_ARROW_LIB=1
 export SF_ARROW_LIBDIR="${PREFIX}/lib"
 export ENABLE_EXT_MODULES=true
