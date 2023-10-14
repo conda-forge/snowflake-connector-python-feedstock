ed29a482d876f9edb67f0dc54f4f067b4f3bb18a
Uwe L. Korn <uwe.korn@quantco.com>
2023-10-14 17:42:20 +0200

    Use C++17 on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,4 +3,5 @@
 set SF_NO_COPY_ARROW_LIB=1
 set "SF_ARROW_LIBDIR=%LIBRARY_LIB%"
 set ENABLE_EXT_MODULES=1
+set CXXFLAGS="%CXXFLAGS% /std:c++17"
 %PYTHON% -m pip install -vvv --no-deps --no-build-isolation .
